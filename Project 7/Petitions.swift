4f26a5a92e0d39ee61142b23fff7689a041f8c0e
keertirajmalik <keertirajmalik@gmail.com>
2022-01-23 20:26:22 +0530

    add one more struct since the result comes inside result array

---
+++ Project 7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by Keertiraj Laxman Malik on 23/01/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
