e7d9c0b81e0c65da7ba548431958f71ddb28bf8b
keertirajmalik <keertirajmalik@gmail.com>
2022-01-23 20:23:17 +0530

    Add the petion struct which tell us the petition json format

---
+++ Project 7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 7
+//
+//  Created by Keertiraj Laxman Malik on 23/01/22.
+//
+
+import Foundation
+
+struct Petition {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
