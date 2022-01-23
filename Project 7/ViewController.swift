319951f46846c6132dac7d81d57dc76d867a64dc
keertirajmalik <keertirajmalik@gmail.com>
2022-01-23 22:33:55 +0530

    add error alert when there is problem in getting data from internet

--- Project 7/ViewController.swift
+++ Project 7/ViewController.swift
@@ -25,8 +25,10 @@ class ViewController: UITableViewController {
         if let url = URL(string: urlString){
             if let data = try? Data(contentsOf: url){
                 parse(json: data)
+                return
             }
         }
+        showError()
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
@@ -45,6 +47,12 @@ class ViewController: UITableViewController {
         let vc = DetailViewController()
         vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 }
 
