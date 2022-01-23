7281ba84508fe1f7c7fa9e9125d6e8cdfd7b5013
keertirajmalik <keertirajmalik@gmail.com>
2022-01-23 20:38:39 +0530

    Converting the data received from internet convert it to our Structs and then display that data

--- Project 7/ViewController.swift
+++ Project 7/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var petitions = [String]()
+    var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +17,7 @@ class ViewController: UITableViewController {
         
         if let url = URL(string: urlString){
             if let data = try? Data(contentsOf: url){
-                //Ready to parse the data
+                parse(json: data)
             }
         }
     }
@@ -27,10 +27,24 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
 
+}
+
+extension ViewController {
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+   
 }
 
